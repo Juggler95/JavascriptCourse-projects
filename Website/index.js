8917bb4277e6bb054660b3a2ebef9ec0a4892720
Juggler95Linux <fireboltisaac@gmail.com>
2025-07-21 16:26:46 -0700

    THIS

--- Website/index.js
+++ Website/index.js
@@ -1,30 +1,21 @@
-// object = a collection of related properties and/or methods
-// can represent real world objects (people, prodects, places)
-// objects = {key:value, function()}
+//this = reference to the object twhere THIS is used
+//        (the object depends on the Immediate context)
+//        person.name = this.name
+
+// this cannot be used with a arrow function
 
 const person1 = {
-  firstName: 'Spongebob',
-  lastName: 'Squarepants',
-  age: 30,
-  isEmployed: true,
-  sayHello: function () {
-    console.log('Hi! I am Spongebob!');
-  },
-  eat: function () {
-    console.log('I am eating a Krabby Patty');
-  },
-};
-
+  name: "Spongebob",
+  favFood: "hamburgers",
+  sayHello: function(){console.log(`Hi! I am ${this.favFood}`)},
+  eat: function(){console.log(`${this.name} is eating ${this.favFood}`)}
+}
 const person2 = {
-  firstName: 'Patrick',
-  lastName: 'Star',
-  age: 42,
-  isEmployed: true,
-  sayHello: () => console.log("Hey, I'm Patrick..."),
-  eat: function () {
-    console.log('I am eating roast beef, chicken, and pizza');
-  },
-};
+  name: "Patrick",
+  favFood: "pizza",
+  sayHello: function(){console.log(`Hi! I am ${this.favFood}`)},
+  eat: function(){console.log(`${this.name} is eating ${this.favFood}`)}
+}
 
-person1.eat();
-person2.eat();
+ person1.eat();
+ person2.eat();
